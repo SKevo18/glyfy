92bea6e12a116d925d781b5af5e73302bcc55fcc
Kevin Svitač <kevin@svit.ac>
2024-09-03 08:26:57 +0000

    oprava ciest

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/bash
 cd "$(dirname "$0")"
 
-if [ ! -d ../.venv ]; then
-  python3 -m venv ../.venv
-  source ../.venv/bin/pip install -r requirements.txt
+if [ ! -d .venv ]; then
+  python3 -m venv .venv
+  source .venv/bin/pip install -r requirements.txt
 fi
 
 export PYTHONPATH=$(pwd)
-../.venv/bin/gunicorn app:APP -m 007 -w 4 --bind=unix:server.sock
+.venv/bin/gunicorn app:APP -m 007 -w 4 --bind=unix:server.sock
